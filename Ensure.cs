e01a91a9d58ce742a81bc94386d6071e5265c04a
SomeDiesYoung <vladimer.osmanoviDOIT@geolab.edu.ge>
2024-10-29 16:23:31 +0400

    adding Ensure.cs and Program.cs

---
+++ Ensure.cs
@@ -0,0 +1,243 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Validation_Methods
+{
+    internal partial class Program
+    {
+        //radgan .NET 6 ar aris INumber<T> gadavwyvite ase gameketebina (ufro kargi gzac mipovnia)
+        public static class Ensure
+        {
+
+
+            //Method IsPositive
+            public static void IsPositive(int number)
+            {
+                if (number <= 0)
+                    throw new ValidationException("The number must be positive.");
+                else
+                {
+                    Console.WriteLine("its okay");
+                }
+            }
+
+            public static void IsPositive(double number)
+            {
+                if (number <= 0.0)
+                    throw new ValidationException("The number must be positive.");
+            }
+
+            public static void IsPositive(short number)
+            {
+                if (number <= 0)
+                    throw new ValidationException("The number must be positive.");
+            }
+
+            public static void IsPositive(long number)
+            {
+                if (number <= 0)
+                    throw new ValidationException("The number must be positive.");
+            }
+
+            public static void IsPositive(decimal number)
+            {
+                if (number <= 0.0m)
+                    throw new ValidationException("The number must be positive.");
+            }
+            public static void IsPositive(float number)
+            {
+                if (number <= 0.0f)
+                    throw new ValidationException("The number must be positive.");
+            }
+
+            //Method IsNegative
+
+            public static void IsNegative(int number)
+            {
+                if (number >= 0)
+                    throw new ValidationException("The number must be Negative.");
+                else
+                {
+                    Console.WriteLine("its okay");
+                }
+            }
+
+            public static void IsNegative(double number)
+            {
+                if (number >= 0.0)
+                    throw new ValidationException("The number must be Negative.");
+            }
+
+            public static void IsNegative(short number)
+            {
+                if (number >= 0)
+                    throw new ValidationException("The number must be Negative.");
+            }
+
+            public static void IsNegative(long number)
+            {
+                if (number >= 0)
+                    throw new ValidationException("The number must be Negative.");
+            }
+
+            public static void IsNegative(decimal number)
+            {
+                if (number >= 0.0m)
+                    throw new ValidationException("The number must be Negative.");
+            }
+            public static void IsNegative(float number)
+            {
+                if (number >= 0.0f)
+                    throw new ValidationException("The number must be Negative.");
+            }
+
+
+            // Method IsNonPositive
+            public static void IsNonPositive(int number)
+            {
+                if (number >= 0)
+                    throw new ValidationException("The number mustnot be Positive.");
+                else
+                {
+                    Console.WriteLine("its okay");
+                }
+            }
+
+            public static void IsNonPositive(double number)
+            {
+                if (number >= 0.0)
+                    throw new ValidationException("The number mustnot be Positive.");
+            }
+
+            public static void IsNonPositive(short number)
+            {
+                if (number >= 0)
+                    throw new ValidationException("The number mustnot be Positive.");
+            }
+
+            public static void IsNonPositive(long number)
+            {
+                if (number >= 0)
+                    throw new ValidationException("The number mustnot be Positive.");
+            }
+
+            public static void IsNonPositive(decimal number)
+            {
+                if (number >= 0.0m)
+                    throw new ValidationException("The number mustnot be Positive.");
+            }
+            public static void IsNonPositive(float number)
+            {
+                if (number >= 0.0f)
+                    throw new ValidationException("The number mustnot be Positive.");
+            }
+
+            //Method IsNonNegative 
+            public static void IsNonNegative(int number)
+            {
+                if (number <= 0)
+                    throw new ValidationException("The number must be Negative .");
+                else
+                {
+                    Console.WriteLine("its okay");
+                }
+            }
+
+            public static void IsNonNegative(double number)
+            {
+                if (number <= 0.0)
+                    throw new ValidationException("The number must be Negative.");
+            }
+
+            public static void IsNonNegative(short number)
+            {
+                if (number <= 0)
+                    throw new ValidationException("The number must be Negative.");
+            }
+
+            public static void IsNonNegative(long number)
+            {
+                if (number <= 0)
+                    throw new ValidationException("The number must be Negative.");
+            }
+
+            public static void IsNonNegative(decimal number)
+            {
+                if (number <= 0.0m)
+                    throw new ValidationException("The number must be Negative.");
+            }
+            public static void IsNonNegative(float number)
+            {
+                if (number <= 0.0f)
+                    throw new ValidationException("The number must be Negative.");
+            }
+
+            // Method IsGreaterThan 
+
+            public static void IsGreaterThan(int number1, int number2)
+            {
+                if (number1 < number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number or equal ({number2}).");
+            }
+            public static void IsGreaterThan(short number1, short number2)
+            {
+                if (number1 < number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number or equal ({number2}).");
+            }
+            public static void IsGreaterThan(long number1, long number2)
+            {
+                if (number1 < number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number ({number2}).");
+            }
+            public static void IsGreaterThan(float number1, float number2)
+            {
+                if (number1 < number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number  ({number2}).");
+            }
+            public static void IsGreaterThan(double number1, double number2)
+            {
+                if (number1 < number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number ({number2}).");
+            }
+            public static void IsGreaterThan(decimal number1, decimal number2)
+            {
+                if (number1 < number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number  ({number2}).");
+            }
+
+            public static void IsGreaterThanOrEqual(int number1, int number2)
+            {
+                if (number1 <= number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number or equal ({number2}).");
+            }
+            public static void IsGreaterThanOrEqual(short number1, short number2)
+            {
+                if (number1 <= number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number or equal ({number2}).");
+            }
+            public static void IsGreaterThanOrEqual(long number1, long number2)
+            {
+                if (number1 <= number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number or equal ({number2}).");
+            }
+            public static void IsGreaterThanOrEqual(float number1, float number2)
+            {
+                if (number1 <= number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number or equal ({number2}).");
+            }
+            public static void IsGreaterThanOrEqual(double number1, double number2)
+            {
+                if (number1 <= number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number or equal ({number2}).");
+            }
+            public static void IsGreaterThanOrEqual(decimal number1, decimal number2)
+            {
+                if (number1 <= number2)
+                    throw new ValidationException($"The first number ({number1}) must be greater than the second number or equal ({number2}).");
+            }
+
+        }
+
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+namespace Validation_Methods
+{
+    internal partial class Program
+    {
+        static void Main()
+        {
+
+            //product class
+            try
+            {
+
+                Product product = new Product(15, -51, "Salmon", "SS-SuperSea", -4);
+                product.Validate();
+                Console.WriteLine("Product is Created");
+            }
+            catch(ValidationException e) {
+                Console.WriteLine(e.Message);
+            }
+
+            //metodebi
+            try
+            {
+                Ensure.IsPositive(5);
+                Ensure.IsPositive(-62.64m);
+
+            }
+            catch (ValidationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                Ensure.IsNegative(55);
+                Ensure.IsNegative(-5);
+            }
+            catch (ValidationException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+
+            try
+            {
+                Ensure.IsGreaterThan(62.03, 62.11);
+            }
+            catch (ValidationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Ensure.IsGreaterThanOrEqual(52, 56);
+            }
+            catch (ValidationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        public class Product
+        {
+            public int Id { get; set; }
+
+            public double Price { get; set; }
+            public string Name { get; set; }
+            public string Manufacturer { get; set; }
+            public float Rating { get; set; }
+
+            public Product(int id, double price, string name, string manufacturer, float rating)
+            {
+                Id = id;
+                Price = price;
+                Name = name;
+                Manufacturer = manufacturer;
+                Rating = rating;
+            }
+
+            public void Validate()
+            {
+                if (Id <= 0)
+                {
+                    throw new ValidationException("Id must be positive integer number");
+                }
+
+                if (Price <= 0)
+                {
+                    throw new ValidationException("Price must be positive number");
+                }
+                if (Name == null)
+                {
+                    throw new ValidationException("name can`t be null");
+                }
+                if (Manufacturer == null) { throw new ValidationException("name can`t be null"); }
+                if (Rating <= 0 || Rating > 5)
+                {
+                    throw new ValidationException("Rating must be in range [0,5]");
+                }
+            }
+        }
+    }
+}
+
